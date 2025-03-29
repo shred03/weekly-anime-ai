836f489c64b9f7d54b96f138ea8e255bb4a97153
shred03 <mithunyada12345@gmail.com>
2025-03-29 20:51:27 +0530

    editing

--- server/script.js
+++ server/script.js
@@ -1,16 +1,16 @@
 const descriptions = {
-    welcome_image: "https://jpcdn.it/img/7a6b735f3168f4903945e5bf3e988aca.png",
-    welcome_text: `🏠 Welcome to K-Drama Hubs Exclusive Bot`,
+    welcome_image: "https://jpcdn.it/img/small/4a5cb323ea7e70f82dea957413fe242d.png",
+    welcome_text: `🏠 Welcome to The Beginning Exclusive Bot`,
 
     // Home section content
-    home: `🏠 Welcome to K-Drama Hubs Exclusive Bot
+    home: `🏠 Welcome to Authur Leywin Era
 
     `,
 
 
     about: `
 
-🤖* Name:* K-Drama Collection
+🤖* Name:* Authur Leywin
 
 📝* Language:* [Node.Js](https://nodejs.org) 
 
@@ -23,25 +23,17 @@ const descriptions = {
 
 *ᴊᴏɪɴ ᴏᴜʀ ᴄʜᴀɴɴᴇʟꜱ.*
     
-*K-Drama:* [K-Drama Hubs](https://t.me/K_DRAMA_HUBS)
-*Request&Chat:* [K-Drama Request & Chats](https://t.me/K_Drama_Request_Chat)
+*Get Anime Here:* [The Beginning After The End](https://t.me/TheBeginningAfterTheEnd_Hindi)
+*Discussion:* [The Beginning After The End Chats](https://t.me/TheBeginningAfterTheEnd_Chat)
 `,
 
     commands: `🔍 *Available Commands*
 
 🧛 Admin Commands:
-• /link or /sl - Single file
-• /batch or /ml - Multiple files
+• /link - Single file
+• /batch - Multiple files
 • /stats - View bot statistics
 • /broadcast - Broadcast message
-• /post movie_name link - Create movies post
-• /setchannel channelId - Set channel to post 
-• /tvpost series_name link - Create series post 
-• /setsticker or /sc channelId/username - Set channel to post.
-
-
-👤 User Commands:
-• /start - Start the bot
 
 *𝘕𝘰𝘵𝘦: 𝘈𝘭𝘭 𝘤𝘰𝘮𝘮𝘢𝘯𝘥𝘴 𝘢𝘳𝘦 𝘸𝘰𝘳𝘬𝘪𝘯𝘨* ✅
 `
